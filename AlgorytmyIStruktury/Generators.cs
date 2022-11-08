2e887be2716d28b7b1c4094dbd35adbc0471975c
Adam Adamczyk <adam.adamczyk@microsoft.wsei.edu.pl>
2022-11-08 23:02:24 +0100

    Dodaj pliki projektów.

---
+++ AlgorytmyIStruktury/Generators.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AlgorytmyIStruktury
+{
+    public static class Generators
+    {
+        public static int[] GenerateRandom(int size, int minVal, int maxVal)
+        {
+            int[] a = new int[size];
+            Random randNum = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = randNum.Next(minVal, maxVal);
+            }
+
+            return a;            
+        }
+
+        public static int[] GenerateSorted(int size, int minVal, int maxVal)
+        {
+            int[] a = GenerateRandom(size, minVal, maxVal);
+            Array.Sort(a);
+            return a;
+        }
+
+        public static int[] GenerateReversed(int size, int minVal, int maxVal)
+        {
+            int[] a = GenerateSorted(size, minVal, maxVal);
+            Array.Reverse(a);
+            return a;
+        }
+    }
+}
---
+++ AlgorytmyIStruktury/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AlgorytmyIStruktury
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            //int[] arr = { 12, 11, 13, 5, 6 };
+            //InsertionSort ob = new InsertionSort();
+            //ob.sort(arr);
+            //ob.printArray(arr);
+            //////////////////////////////////////////////////////////////////////////////////////
+
+            //
+            //int[] arr2 = { 12, 11, 13, 5, 6, 7 };
+            //Console.WriteLine("Given Array");
+            //MergeSort ms = new MergeSort();
+            //ms.printArray(arr2);
+            //ms.sort(arr2, 0, arr2.Length - 1);
+            //Console.WriteLine("\nSorted array");
+            //ms.printArray(arr2);
+            //////////////////////////////////////////////////////////////////////////////////////
+
+            //
+            //int[] arr3 = { 10, 7, 8, 9, 1, 5 };
+            //int n = arr3.Length;
+            //QuickSortClassic qsc = new QuickSortClassic();
+            //qsc.quickSort(arr3, 0, n - 1);
+            //Console.WriteLine("Sorted array: ");
+            //qsc.printArray(arr3, n);
+            //////////////////////////////////////////////////////////////////////////////////////
+
+            int[] a = Generators.GenerateRandom(size: 10, minVal: 0, maxVal: 20);
+            foreach (int entry in a)
+                Console.Write(entry + " ");
+
+            Console.WriteLine();
+
+            int[] a2 = Generators.GenerateSorted(size: 10, minVal: 0, maxVal: 20);
+            foreach (int entry in a2)
+                Console.Write(entry + " ");
+
+            Console.WriteLine();
+
+            int[] a3 = Generators.GenerateReversed(size: 10, minVal: 0, maxVal: 20);
+            foreach (int entry in a3)
+                Console.Write(entry + " ");
+        }
+    }
+}
